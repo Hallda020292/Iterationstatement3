abf0cf7d4750b274a0eded1e95ed5113d9cc4c3b
Dakota hall <Hallda020292@gmail.com>
2022-11-19 11:16:39 -0500

    initial commit

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -2,32 +2,96 @@
 {
     public class Program
     {
-        //LukeWarm Section: Create methods below
-        //Write a method that will print to the console all numbers 1000 through - 1000
-
-        //Write a method that will print to the console numbers 3 through 999 by 3 each time
-
-        //Write a method to accept two integers as parameterss and check whether they are equal or not
-        
-        //Write a method to check whether a given number is even or odd
-        
-        //Write a method to check whether a given number is positive or negative
-        
-        //Write a method to read the age of a candidate and determine whether they can vote.
-        //Hint: Use Parse or the safer TryParse() for an extra challenge
-        //Parse()
-        //TryParse()
-
-        //Heatin Up Section:
-        //Write a method to check if an integer(from the user) is in the range -10 to 10
-        
-        //Write a method to display the multiplication table(from 1 to 12) of a given integer
-
-
-        //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
-            
+
+            Console.WriteLine();
+            Console.Read();
+            Console.WriteLine(CounterTwo(2, 8));
+            MultiplicationTable(3);
+        }
+
+        public static bool CounterTwo(int a, int b)
+        {
+            var check = a == b ? true : false;
+
+            return check;
+        }
+
+
+
+
+
+        public static void Threes()
+        {
+            for (int k = 3; k <= 999; k += 3)
+            {
+                Console.WriteLine(k);
+            }
+        }
+
+
+        public static void PrintRange(int ceiling, int floor)
+        {
+            for (int i = ceiling; i >= floor; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void EvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"thats definitely even");
+            }
+            else
+            {
+                Console.WriteLine($"thats definitely odd");
+            }
+
+        }
+
+        public static bool IsPositive(int num1)
+        {
+            if (num1 > 0)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public static bool OfVotingAge(int age)
+        {
+            if (age >= 18)
+            {
+                Console.WriteLine($"yes they can vote");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"No too young to vote by {18 - age} years");
+                return false;
+            }
+        }
+
+        public static bool IsTenRange(int num)
+        {
+            if (num <= 10 && num >= -10)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void MultiplicationTable(int num)
+        {
+            for (var i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{num} times {i} is {num * i}");
+            }
         }
     }
 }
